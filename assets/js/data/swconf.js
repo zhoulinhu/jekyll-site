0f46d1e75bf75b488fe1771076c470c7bd7a6107
zhoulinhu <mailbox_of_zlh@163.com>
2026-06-13 18:25:50 +0800

    first commit

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1781337775',resources: [
+      '/jekyll-site/assets/css/jekyll-theme-chirpy.css',
+      '/jekyll-site/',
+      
+        '/jekyll-site/categories/',
+      
+        '/jekyll-site/tags/',
+      
+        '/jekyll-site/archives/',
+      
+        '/jekyll-site/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
